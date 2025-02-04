504f727671db5ba8c4bd4fafba42044c4384d0da
Jeremy DeLong <jdelong@backofficethinking.com>
2025-02-03 23:52:09 -0500

    Update ConfluenceApiClient.php

--- src/ConfluenceApiClient.php
+++ src/ConfluenceApiClient.php
@@ -18,29 +18,28 @@ class ConfluenceApiClient {
     $this->cache = $cache;
   }
 
-  public function getSpaceContent($spaceKey, $limit = 25, $start = 0) {
-    $cacheKey = "confluence_space_content_{$spaceKey}_{$start}_{$limit}";
-    if ($this->cache && $cachedData = $this->cache->get($cacheKey)) {
-      return $cachedData;
+public function getSpaceContent($spaceKey, $limit = 25, $start = 0) {
+  $cacheKey = "confluence_space_content_{$spaceKey}_{$start}_{$limit}";
+  if ($this->cache && $cachedData = $this->cache->get($cacheKey)) {
+    return $cachedData;
+  }
+
+  $url = $this->baseUri . '/rest/api/content?spaceKey=' . $spaceKey . "&limit=$limit&start=$start&expand=body.view";
+  try {
+    $response = $this->client->get($url, [
+      'headers' => [
+        'Authorization' => 'Basic ' . $this->authToken,
+        'Accept' => 'application/json',
+      ],
+    ]);
+    $data = json_decode($response->getBody(), true);
+
+    if ($this->cache) {
+      $this->cache->set($cacheKey, $data);
     }
 
-    $url = $this->baseUri . '/rest/api/content?spaceKey=' . $spaceKey . "&limit=$limit&start=$start";
-    try {
-      $response = $this->client->get($url, [
-        'headers' => [
-          'Authorization' => 'Basic ' . $this->authToken,
-          'Accept' => 'application/json',
-        ],
-      ]);
-      $data = json_decode($response->getBody(), true);
-
-      if ($this->cache) {
-        $this->cache->set($cacheKey, $data);
-      }
-
-      return $data;
-    } catch (RequestException $e) {
-      throw new \Exception('Error fetching Confluence content: ' . $e->getMessage());
-    }
+    return $data;
+  } catch (RequestException $e) {
+    throw new \Exception('Error fetching Confluence content: ' . $e->getMessage());
   }
 }
